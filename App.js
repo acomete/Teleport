bca508e8e3f14bdfc5147234ad755837bf4193b4
Pierre-Gérard David <pierre-gerard.david@doc.fr>
2021-05-20 02:21:31 +0200

    Fullscreen video

--- App.js
+++ App.js
@@ -5,57 +5,80 @@ import Agent from './Agent';
 
 export default function App() {
     const [play, setPlay] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [selected, setSelected] = useState(null);
 
     return (
         <>
-            <Modal
-                visible={play}
-                transparent={true}
-                animationType="slide"
-            >
-                <View style={styles.modal}>
-                    <Video360
-                        urlVideo={'https://www.6science.fr/voulez-vous.mp4'}
-                        modeVideo={1}
-                        enableCardboardButton={false}
-                        enableInfoButton={false}
-                        style={styles.vr}
-                    />
-                    <TouchableHighlight
-                        style={styles.closeButton}
-                        onPress={() => {
-                            setPlay(false);
-                        }}>
+            {play && <View
+                style={{ flex: 1 }}>
+                <TouchableHighlight
+                    style={styles.closeButton}
+                    onPress={() => {
+                        setPlay(false);
+                    }}>
+                    <View style={{ flex: 1, flexDirection: 'row' }}>
                         <Text style={styles.closeButtonText}>Se déconnecter</Text>
+                        <View style={styles.online}></View>
+                    </View>
+                </TouchableHighlight>
+                <Video360
+                    urlVideo={'https://www.6science.fr/voulez-vous.mp4'}
+                    modeVideo={1}
+                    enableCardboardButton={false}
+                    enableInfoButton={false}
+                    style={{ flex: 1 }}
+                />
+            </View>
+            }
+            {!play && <>
+                <Modal
+                    visible={loading}
+                    transparent={true}
+                    animationType="slide"
+                >
+                    <View style={styles.modal}>
+                        <Image source={require('./src/assets/images/teleporter.jpg')} style={{
+                            width: '100%', borderRadius: 25, resizeMode: 'cover', height: '100%'
+                        }}/>
+                        <View style={styles.closeButton}>
+                            <View style={{ flex: 1, flexDirection: 'row' }}>
+                                <Text style={styles.closeButtonText}>Téléportation ...</Text>
+                                <View style={styles.online}></View>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+                <View style={styles.container}>
+                    <Image style={styles.logo} source={require('./src/assets/images/teleport-logo.png')}/>
+                    <Text style={styles.text}>(912) agents sur le terrain</Text>
+                    <ScrollView style={styles.agents}>
+                        <Agent onPress={() => setSelected(2)} selected={selected === 2} name={'Amine'} location={'Paris'} coords={'48.86, 2.35'} face={2} online={false}/>
+                        <Agent onPress={() => setSelected(3)} selected={selected === 3} name={'Malaïka'} location={'Paris'} coords={'48.12, 2.80'} face={3} online={false}/>
+                        <Agent onPress={() => setSelected(5)} selected={selected === 5} name={'Rawad'} location={'Paris'} coords={'48.52, 2.43'} face={5} online={false}/>
+                        <Agent onPress={() => setSelected(7)} selected={selected === 7} name={'Pablo'} location={'Pontoise'} coords={'43.29, 6.02'} face={7} online={false}/>
+                        <Agent onPress={() => setSelected(8)} selected={selected === 8} name={'Zineb'} location={'Massy'} coords={'43.98, 4.22'} face={8} online={false}/>
+                        <Agent onPress={() => setSelected(4)} selected={selected === 4} name={'Clémence'} location={'Marseilles'} coords={'43.32, 5.41'} face={4} online={false}/>
+                        <Agent onPress={() => setSelected(1)} selected={selected === 1} name={'Rudolphe'} location={'Bordeaux'} coords={'44.81, -0.61'} face={1} online={true}/>
+                        <Agent onPress={() => setSelected(6)} selected={selected === 6} name={'Raymond'} location={'Chantilly'} coords={'42.56, 5.32'} face={6} online={false}/>
+                    </ScrollView>
+                    <TouchableHighlight
+                        style={selected === 1 ? styles.openButton : styles.disabled}
+                        onPress={() => {
+                            if (selected === 1) {
+                                setLoading(true);
+
+                                setTimeout((() => {
+                                    setLoading(false);
+                                    setPlay(true);
+                                }), 3000);
+                            }
+                        }}>
+                        <Text style={styles.openButtonText}>{selected === 1 ? 'Se téléporter' : !selected ? '...' : 'Agent inactif'}</Text>
                     </TouchableHighlight>
                 </View>
-            </Modal>
-            <View style={styles.container}>
-                <Image style={styles.logo} source={require('./src/assets/images/teleport-logo.png')}/>
-                <Text style={styles.text}>(912) agents sur le terrain</Text>
-                <ScrollView style={styles.agents}>
-                    <Agent onPress={() => setSelected(2)} selected={selected === 2} name={'Amine'} location={'Paris'} coords={'48.86, 2.35'} face={2} online={false}/>
-                    <Agent onPress={() => setSelected(3)} selected={selected === 3} name={'Malaïka'} location={'Paris'} coords={'48.12, 2.80'} face={3} online={false}/>
-                    <Agent onPress={() => setSelected(5)} selected={selected === 5} name={'Rawad'} location={'Paris'} coords={'48.52, 2.43'} face={5} online={false}/>
-                    <Agent onPress={() => setSelected(7)} selected={selected === 7} name={'Pablo'} location={'Pontoise'} coords={'43.29, 6.02'} face={7} online={false}/>
-                    <Agent onPress={() => setSelected(8)} selected={selected === 8} name={'Zineb'} location={'Massy'} coords={'43.98, 4.22'} face={8} online={false}/>
-                    <Agent onPress={() => setSelected(4)} selected={selected === 4} name={'Clémence'} location={'Marseilles'} coords={'43.32, 5.41'} face={4} online={false}/>
-                    <Agent onPress={() => setSelected(1)} selected={selected === 1} name={'Rudolphe'} location={'Bordeaux'} coords={'44.81, -0.61'} face={1} online={true}/>
-                    <Agent onPress={() => setSelected(6)} selected={selected === 6} name={'Raymond'} location={'Chantilly'} coords={'42.56, 5.32'} face={6} online={false}/>
-                </ScrollView>
-                <TouchableHighlight
-                    style={selected === 1 ? styles.openButton : styles.disabled}
-                    onPress={() => {
-                        if (selected === 1) {
-                            setPlay(true);
-                        }
-                    }}>
-                    <Text style={styles.openButtonText}>{selected === 1 ? 'Se téléporter' : !selected ? '...' : 'Agent inactif'}</Text>
-                </TouchableHighlight>
-            </View>
-        </>
-    );
+            </>}
+        </>);
 }
 
 const styles = StyleSheet.create({
@@ -66,10 +89,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     modal: {
-        backgroundColor: '#222',
+        backgroundColor: '#222222',
         flex: 1,
-        marginHorizontal: 10,
-        marginVertical: 20,
+        marginHorizontal: 0,
+        marginVertical: 0,
         flexDirection: 'column',
         borderRadius: 15,
     },
@@ -98,13 +121,15 @@ const styles = StyleSheet.create({
     },
     closeButton: {
         position: 'absolute',
+        alignSelf: 'center',
         bottom: 15,
-        left: 50,
         backgroundColor: '#000',
         borderRadius: 12,
         padding: 10,
         elevation: 2,
-        width: 250,
+        width: 175,
+        zIndex: 999,
+        paddingLeft: 15,
     },
     openButtonText: {
         color: '#FFF',
@@ -119,19 +144,20 @@ const styles = StyleSheet.create({
     text: {
         color: '#FFF',
     },
-    vr: {
-        flex: 1,
-        marginHorizontal: 5,
-        marginBottom: 60,
-        marginTop: 5,
-        resizeMode: 'cover',
-        /*transform: [{ rotateZ: '90deg' }]*/
-    },
     agents: {
         flex: 1,
         width: '100%',
         paddingHorizontal: 40,
         flexDirection: 'column',
         marginTop: 15,
+    },
+    online: {
+        position: 'absolute',
+        right: 10,
+        top: 5,
+        width: 10,
+        height: 10,
+        borderRadius: 5,
+        backgroundColor: '#b2ff00',
     },
 });
